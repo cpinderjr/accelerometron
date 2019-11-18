893501495bbcf32246f4f79a33b3514aa085f9a4
Carlos Pinder <carlos.pinder@icloud.com>
2019-11-18 12:12:59 -0500

    first commit

---
+++ accelerometron/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  accelerometron
+//
+//  Created by Carlos Pinder on 11/18/19.
+//  Copyright © 2019 Carlos Pinder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
